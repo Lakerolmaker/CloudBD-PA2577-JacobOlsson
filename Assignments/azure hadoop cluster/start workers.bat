44e4ae01ffb78d19b2dc277a0fef4c572f125e5b
lakerolmaker <jacob.n.olsson@hotmail.com>
2021-09-29 13:00:27 +0200

    Added readme to hadoop cluster

--- Assignments/azure hadoop cluster/start workers.bat
+++ Assignments/azure hadoop cluster/start workers.bat
@@ -1,5 +1,5 @@
 start cmd.exe /k "vagrant up worker0 --provider=azure"
-PAUSE
+::PAUSE
 start cmd.exe /k "vagrant up worker1 --provider=azure"
-PAUSE
+::PAUSE
 start cmd.exe /k "vagrant up worker2 --provider=azure"
